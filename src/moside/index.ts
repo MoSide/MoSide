5164349294e40f6806aeec528d1168f044b5242e
yskun <yskun@outlook.com>
2019-05-18 17:56:54 +0800

    [modified] export Ctx

--- src/moside/index.ts
+++ src/moside/index.ts
@@ -1,14 +1,16 @@
-import {Moside} from './moside'
-import {ControllerOptions, IControllerMetadata, IControllerMethodMetadata} from './controller.interface'
-import {IHttpMethod} from './method.interface'
-import {MethodCtx} from './util/method-ctx'
-import {Controller, getControllerMetadata} from './decorators/Controller'
-import {Del, Get, getControllerMethodMetadata, Head, Http, HttpMethod, Post, Put} from './decorators/HttpMethod'
+import { Moside } from './moside'
+import { ControllerOptions, IControllerMetadata, IControllerMethodMetadata } from './controller.interface'
+import { IHttpMethod } from './method.interface'
+import { MethodCtx } from './util/method-ctx'
+import { Ctx } from './ctx'
+import { Controller, getControllerMetadata } from './decorators/Controller'
+import { Del, Get, getControllerMethodMetadata, Head, Http, HttpMethod, Post, Put } from './decorators/HttpMethod'
 
 
 export {Moside}
 export {ControllerOptions, IControllerMetadata, IControllerMethodMetadata}
 export {IHttpMethod}
 export {MethodCtx}
+export { Ctx }
 export {Controller, getControllerMetadata}
 export {Http, HttpMethod, Get, Post, Put, Del, Head, getControllerMethodMetadata}
